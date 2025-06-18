ed61d8c85d5047e95135f3fe32c02cb6a68d6531
Pedro Castro <pedro.castro@sou.inteli.edu.br>
2025-06-18 01:51:49 -0300

    Hotfix: Corrige erro de conexão com o banco de dados

--- src/config/db.js
+++ src/config/db.js
@@ -1,17 +1,15 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-
 const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
-    ssl: false
-  });
+    ssl: {
+        rejectUnauthorized: false
+    },
+});
 
-module.exports = {
-  query: (text, params) => pool.query(text, params),
-  connect: () => pool.connect(),
-};
+module.exports = pool;
